65f6091d55063569369b66a7ac7d26b6ebb2bb0b
Lou-Anne Tilmant <lou.tlmt04@gmail.com>
2026-01-21 17:10:50 +0100

    add audio output with dB

--- src/gui_output.c
+++ src/gui_output.c
@@ -16,55 +16,88 @@ static const char *NomForme(FormeOnde f)
     }
 }
 
+static float DbEstime(float volume01)
+{
+    if (volume01 < 1e-6f) return -60.0f;
+    return 20.0f * log10f(volume01);
+}
+
+static void DrawCard(int x, int y, int w, int h, float dpi,
+                     const char *label, const char *value, Color valueColor)
+{
+    Rectangle r = { (float)x, (float)y, (float)w, (float)h };
+    DrawRectangleRec(r, (Color){235,235,235,255});
+    DrawRectangleLinesEx(r, 2, (Color){160,160,160,255});
+
+    DrawText(label, x + (int)(12*dpi), y + (int)(10*dpi), (int)(14*dpi), GRAY);
+    DrawText(value, x + (int)(12*dpi), y + (int)(34*dpi), (int)(18*dpi), valueColor);
+}
+
+static void DrawBar(int x, int y, int w, int h, float dpi, float t01, Color fill)
+{
+    if (t01 < 0.0f) t01 = 0.0f;
+    if (t01 > 1.0f) t01 = 1.0f;
+
+    Rectangle r = { (float)x, (float)y, (float)w, (float)h };
+    DrawRectangleRec(r, (Color){30,30,30,255});
+    DrawRectangleLinesEx(r, 2, (Color){120,120,120,255});
+
+    Rectangle rf = { r.x + 2, r.y + 2, (r.width - 4) * t01, r.height - 4 };
+    DrawRectangleRec(rf, fill);
+}
+
 void DessinerPageOutput(AppState *etat, int zoneX)
 {
     float dpi = GetAppDPI();
 
-    int x = zoneX + 20*dpi;
-    int y = 30*dpi;
-    int w = GetScreenWidth() - zoneX - 40*dpi;
+    int x = zoneX + (int)(20*dpi);
+    int y = (int)(30*dpi);
+    int w = GetScreenWidth() - zoneX - (int)(40*dpi);
 
-    DrawText("CONFIGURATION ACTUELLE", x, y, 18*dpi, DARKGRAY);
-    y += 25*dpi;
+    DrawText("OUTPUT (MONO)", x, y, (int)(20*dpi), DARKGRAY);
+    y += (int)(30*dpi);
 
-    int boxH = 65*dpi;
-    int gap  = 15*dpi;
-    int colW = (w - 15*dpi) / 2;
+    int gap = (int)(15*dpi);
+    int boxH = (int)(65*dpi);
+    int colW = (w - gap) / 2;
 
-    Rectangle rForme  = { x, y, colW, boxH };
-    Rectangle rFreq   = { x + colW + 15*dpi, y, colW, boxH };
-    Rectangle rVol    = { x, y + boxH + gap, colW, boxH };
-    Rectangle rEtat   = { x + colW + 15*dpi, y + boxH + gap, colW, boxH };
+    char vForme[64], vFreq[64], vVol[64], vEtat[64];
 
-    Rectangle boxes[4] = { rForme, rFreq, rVol, rEtat };
+    snprintf(vForme, sizeof(vForme), "%s", NomForme(etat->formeOnde));
+    snprintf(vFreq,  sizeof(vFreq),  "%.0f Hz", etat->frequenceHz);
+    snprintf(vVol,   sizeof(vVol),   "%.0f %%", etat->volume * 100.0f);
 
-    for (int i = 0; i < 4; i++) {
-        DrawRectangleRec(boxes[i], (Color){235,235,235,255});
-        DrawRectangleLinesEx(boxes[i], 2, (Color){160,160,160,255});
-    }
+    const char *playTxt = etat->lecture ? "PLAY" : "STOP";
+    const char *audioTxt = etat->audioActif ? "ON" : "OFF";
+    snprintf(vEtat, sizeof(vEtat), "%s | AUDIO %s", playTxt, audioTxt);
 
-    DrawText("Forme d'onde", rForme.x + 12*dpi, rForme.y + 10*dpi, 14*dpi, GRAY);
-    DrawText(NomForme(etat->formeOnde),
-             rForme.x + 12*dpi, rForme.y + 34*dpi, 18*dpi, BLACK);
+    DrawCard(x,              y, colW, boxH, dpi, "Forme",     vForme, BLACK);
+    DrawCard(x + colW + gap, y, colW, boxH, dpi, "Fréquence", vFreq,  BLACK);
 
-    char buf[32];
-    DrawText("Fréquence", rFreq.x + 12*dpi, rFreq.y + 10*dpi, 14*dpi, GRAY);
-    snprintf(buf, sizeof(buf), "%.0f Hz", etat->frequenceHz);
-    DrawText(buf, rFreq.x + 12*dpi, rFreq.y + 34*dpi, 18*dpi, BLACK);
+    y += boxH + gap;
 
-    DrawText("Volume", rVol.x + 12*dpi, rVol.y + 10*dpi, 14*dpi, GRAY);
-    snprintf(buf, sizeof(buf), "%.0f %%", etat->volume * 100.0f);
-    DrawText(buf, rVol.x + 12*dpi, rVol.y + 34*dpi, 18*dpi, BLACK);
+    DrawCard(x,              y, colW, boxH, dpi, "Volume",    vVol,   BLACK);
 
-    DrawText("État", rEtat.x + 12*dpi, rEtat.y + 10*dpi, 14*dpi, GRAY);
-    const char *etatTxt = etat->audioActif ? GuiIconText(ICON_PLAYER_PLAY, "EN LECTURE") : GuiIconText(ICON_PLAYER_STOP, "ARRÊTÉ");
-    Color c = etat->audioActif ? (Color){60,170,90,255} : (Color){210,80,80,255};
+    Color cEtat = etat->audioActif ? (Color){60,170,90,255} : (Color){210,80,80,255};
+    DrawCard(x + colW + gap, y, colW, boxH, dpi, "État",      vEtat,  cEtat);
 
-    int prevColor = GuiGetStyle(LABEL, TEXT_COLOR_NORMAL);
-    GuiSetStyle(LABEL, TEXT_COLOR_NORMAL, ColorToInt(c));
-    GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_LEFT);
+    y += boxH + (int)(25*dpi);
 
-    GuiLabel((Rectangle){rEtat.x + 10*dpi, rEtat.y + 34*dpi, rEtat.width, 24*dpi}, etatTxt);
+    DrawText("Niveau (estimé)", x, y, (int)(16*dpi), DARKGRAY);
+    y += (int)(22*dpi);
 
-    GuiSetStyle(LABEL, TEXT_COLOR_NORMAL, prevColor);
+    float db = DbEstime(etat->volume);
+    char vDb[64];
+    snprintf(vDb, sizeof(vDb), "%.1f dB", db);
+    DrawText(vDb, x, y, (int)(18*dpi), BLACK);
+
+    y += (int)(28*dpi);
+
+    DrawBar(x, y, w, (int)(18*dpi), dpi, etat->audioActif ? etat->volume : 0.0f,
+            etat->audioActif ? (Color){80,180,120,255} : (Color){200,70,70,255});
+
+    y += (int)(30*dpi);
+
+    DrawText("Format cible : Mono - 44100 Hz - Float32", x, y, (int)(14*dpi), GRAY);
+    y += (int)(18*dpi);
 }
